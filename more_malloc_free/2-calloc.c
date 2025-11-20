8098b5593f1e87ed56ff6391fc9fc83095856da2
Mxn-ptr <maxence0@hotmail.fr>
2025-11-20 09:11:31 +0100

    Add task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory for an array and initialized it to zero
+ * @nmemb: number of elements of the array
+ * @size: size in bytes of each element of the array
+ * 
+ * Return: pointer to the new allocated area or NULL if it fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned char *ptr = NULL;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
